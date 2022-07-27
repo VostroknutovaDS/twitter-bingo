94cb9ecf2fa7ff14bd4c8bbcb6f4b6745c666c43
Darya Vostroknutova <justdsarap@gmail.com>
2022-07-27 16:57:03 +0300

    feat: add table generation in template

--- src/app/bingo-preview/bingo-preview.component.ts
+++ src/app/bingo-preview/bingo-preview.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MOCK_TABLE } from 'src/assets/configuration/mock-data';
+import { BingoElement } from '../core/types';
 
 @Component({
   selector: 'app-bingo-preview',
   templateUrl: './bingo-preview.component.html',
-  styleUrls: ['./bingo-preview.component.scss']
+  styleUrls: ['./bingo-preview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BingoPreviewComponent implements OnInit {
+  private generatedTable: BingoElement[][] = MOCK_TABLE;
+
+  public get GeneratedTable(): BingoElement[][] {
+    return this.generatedTable;
+  }
+
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
