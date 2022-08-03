0f76731542f08ff2473c9e5645e002be4fb8c710
Darya Vostroknutova <justdsarap@gmail.com>
2022-08-03 14:01:14 +0300

    feat: add table generation from list of elements

--- src/app/core/types.ts
+++ src/app/core/types.ts
@@ -1,4 +1,5 @@
 export type BingoElement = {
     id: string,
     value: string,
+    isGenerated?: boolean
 };
--- src/app/data/table-generation.service.ts
+++ src/app/data/table-generation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { MAX_NUMBER_OF_ELEMENTS, MAX_NUMBER_OF_ROWS } from 'src/assets/configuration/constants';
 import { ElementsBaseService } from '../core/elements-base.service';
 import { TableGenerationBaseService } from '../core/table-generation-base.service';
 import { BingoElement } from '../core/types';
@@ -9,6 +10,7 @@ import { BingoElement } from '../core/types';
 })
 export class TableGenerationService implements TableGenerationBaseService {
   private table$: Subject<BingoElement[][]> = new Subject<BingoElement[][]>();
+  private table: BingoElement[][] = [];
 
   constructor(private readonly elementsService: ElementsBaseService) {
     this.elementsService.getElements().subscribe(elements => {
@@ -21,7 +23,38 @@ export class TableGenerationService implements TableGenerationBaseService {
   }
 
   private generateTable(elements: BingoElement[]): void {
-    //transform elements into table and add to observable
-    this.table$.next([]);
+    if (elements.length > MAX_NUMBER_OF_ELEMENTS) {
+      return;
+    }
+
+    const rows: number = this.findRowsNumber(elements);
+    const newElements = this.addEmptyCells(elements, rows);
+
+    this.table = [];
+    for (let i = 0; i < rows; i++) {
+      this.table.push(newElements.slice(rows * i, rows * (i + 1)));
+    }
+
+    this.table$.next(this.table);
+  }
+
+  private findRowsNumber(elements: Array<unknown>): number {
+    if (elements.length === 0) {
+      return 0;
+    }
+
+    let rows = 0;
+    let found = false;
+    while (rows <= MAX_NUMBER_OF_ROWS && !found) {
+      rows++;
+      found = elements.length <= rows * rows;
+    }
+
+    return rows;
+  }
+
+  private addEmptyCells(elements: BingoElement[], rows: number): BingoElement[] {
+    const emptyCells: BingoElement[] = Array(rows * rows - elements.length).fill(<BingoElement>{ value: '', isGenerated: true });
+    return elements.concat(emptyCells);
   }
 }
