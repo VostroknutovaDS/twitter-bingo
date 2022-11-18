9bedd6b56e854b5abc2fbddafe377dea32ca851e
Darya Vostroknutova <justdsarap@gmail.com>
2022-11-18 12:28:14 +0100

    feat: add isMobile check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  public isMobile = false;
+
+  ngOnInit(): void {
+    this.isMobile = navigator.userAgent.includes('Mobi');
+    sessionStorage.setItem('isMobile', this.isMobile.toString());
+  }
+}
