4fcf57ee9bedb9997957b85974408943f2042b48
Darya Vostroknutova <justdsarap@gmail.com>
2023-02-21 22:51:27 +0100

    Auto-generated commit [ci skip]

--- assets/configuration/providers.ts
+++ assets/configuration/providers.ts
@@ -1,8 +1,10 @@
 import { Provider } from "@angular/core";
 import { ElementsBaseService } from "src/app/core/elements-base.service";
+import { FeatureToggleServiceBase } from "src/app/core/feature-toggle-base.service";
 import { TableGenerationBaseService } from "src/app/core/table-generation-base.service";
 import { ElementsService } from "src/app/data/elements.service";
 import { TableGenerationService } from "src/app/data/table-generation.service";
+import { FeatureToggleService } from "src/app/data/feature-toggle.service";
 
 export const DATA_PROVIDERS: Provider[] = [
     {
@@ -12,5 +14,9 @@ export const DATA_PROVIDERS: Provider[] = [
     {
         provide: TableGenerationBaseService,
         useClass: TableGenerationService
+    },
+    {
+        provide: FeatureToggleServiceBase,
+        useClass: FeatureToggleService
     }
 ];
