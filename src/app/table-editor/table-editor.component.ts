7baa53a81612da45a03b9ffe0dae88f682e50ff3
Darya Vostroknutova <justdsarap@gmail.com>
2023-02-26 23:38:49 +0100

    docs: add description to table editor component

--- src/app/table-editor/table-editor.component.ts
+++ src/app/table-editor/table-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/**
+ * The component that displays the table editor menu
+ */
 @Component({
   selector: 'app-table-editor',
   templateUrl: 'table-editor.component.html',
