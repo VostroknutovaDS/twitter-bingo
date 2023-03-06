c6b5d2fee661a5b1d9966d009bc8728b4ac6dc5c
Darya Vostroknutova <justdsarap@gmail.com>
2023-03-06 22:14:49 +0100

    feat: add table editor basic view

--- src/app/table-editor/table-editor.component.ts
+++ src/app/table-editor/table-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 /**
  * The component that displays the table editor menu
@@ -6,10 +7,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-table-editor',
   templateUrl: 'table-editor.component.html',
+  styleUrls: ['table-editor.component.scss'],
 })
 export class TableEditorComponent {
   /*TODO
-  Table should have these configurations enbled:
-  1) Background: set your own image or use samples
-  2) Border: size, color, type */
+  Table should have these configurations enabled:
+  1) Background: set your own image or use samples(colors, images)
+  2) Border: size, color, style?
+  3) Title style: font, font size, bold/italic
+  4) Cells: background color, text color, font, font size, bold/italic
+  */
+
+  public titleControls = new FormGroup({
+    fontControl: new FormControl(),
+    fontSizeControl: new FormControl(),
+    fontStyleControl: new FormControl(),
+  });
+  public backgroundControl = new FormGroup({});
+  public borderControl = new FormGroup({
+    sizeControl: new FormControl(),
+    colorControl: new FormControl(),
+  });
+  public cellsControl = new FormGroup({
+    backgroundControl: new FormControl(),
+    textColorControl: new FormControl(),
+    fontControl: new FormControl(),
+    fontSizeControl: new FormControl(),
+    fontStyleControl: new FormControl(),
+  });
 }
